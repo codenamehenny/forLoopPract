9e35c40e2aef782ba8b521d58f8b8662bd7baa1a
codenamehenny <genesis09m@hotmail.com>
2024-07-23 12:41:45 -0400

    nested for loop for festival planning scenario

--- LoopFestPlan.py
+++ LoopFestPlan.py
@@ -1,8 +1,10 @@
 # Utilize for loops to iterate over multiple lists and organize event details without using functions.
 
+# 2 lists for event type and music type
 events = ["BBQ", "family reunion", "birthday party", "Thanksgiving dinner", "New Years Eve party"]
 music = ["disco", "throwbacks", "hip-hop", "Bad Bunny"]
 
+# nested for loop with combination between the 2 lists
 for event in events:
     for genre in music:
         print("For a " + event +  ", play some " + genre + "!")
